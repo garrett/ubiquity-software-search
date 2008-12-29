14b87b6efd0ec5641e9735776ed5e7b211bcec38
Garrett LeSage <garrett@novell.com>
2008-12-29 13:02:43 -0500

    openSUSE:10.3

--- software-search.js
+++ software-search.js
@@ -11,7 +11,7 @@ CmdUtils.CreateCommand({
     var url = {
       // XHR response URL
       template: "http://software.opensuse.org/search/search?baseproject={BASE}&commit=Search&q={QUERY}",
-      base: "ALL",
+      base: "openSUSE:10.3",
       query: directObject.text
     };
 

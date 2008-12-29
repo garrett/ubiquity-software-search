046794b9a55f4fcb850788ee95fadafc1741c431
Garrett LeSage <garrett@novell.com>
2008-12-29 13:06:36 -0500

    Fedora:8

--- software-search.js
+++ software-search.js
@@ -11,7 +11,7 @@ CmdUtils.CreateCommand({
     var url = {
       // XHR response URL
       template: "http://software.opensuse.org/search/search?baseproject={BASE}&commit=Search&q={QUERY}",
-      base: "ALL",
+      base: "Fedora:8",
       query: directObject.text
     };
 

2b7e9ad8b5b7c81fa3cc804d0005beed413199fe
Garrett LeSage <garrett@novell.com>
2008-12-29 13:08:11 -0500

    CentOS:CentOS-5

--- software-search.js
+++ software-search.js
@@ -11,7 +11,7 @@ CmdUtils.CreateCommand({
     var url = {
       // XHR response URL
       template: "http://software.opensuse.org/search/search?baseproject={BASE}&commit=Search&q={QUERY}",
-      base: "ALL",
+      base: "CentOS:CentOS-5",
       query: directObject.text
     };
 
